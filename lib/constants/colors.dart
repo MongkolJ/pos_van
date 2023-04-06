6fe73f522c1232ab1a2b38a278a29e2d3f98a256
mongkoljitwasin <mongkol.jitwasin@gmail.com>
2023-04-06 09:41:36 +0700

    Add preset colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryDarkColor = Color.fromRGBO(14, 78, 137, 1);
+const Color kPrimaryLightColor = Color.fromRGBO(0, 102, 197, 1);
+
+const Color kBackgroundColor = Color.fromRGBO(245, 245, 245, 1);
+const Color kCardBackgroundColor = Color.fromRGBO(250, 250, 251, 1);
+
+const Color kFadedTextColor = Color.fromRGBO(111, 128, 154, 1);
