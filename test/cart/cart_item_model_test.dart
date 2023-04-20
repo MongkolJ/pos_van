dad131f270247846ac1ecaf30475984a492f6934
mongkoljitwasin <mongkol.jitwasin@gmail.com>
2023-04-20 21:20:24 +0700

    Update CartItemModel Unit Test

--- test/cart/cart_item_model_test.dart
+++ test/cart/cart_item_model_test.dart
@@ -20,7 +20,7 @@ void main() {
   test('Request total price', () {
     int amount = sut.amount;
     double price = sut.price;
-    expect(sut.totalPrice(), amount * price);
+    expect(sut.totalPrice(), (amount * price).toString());
   });
 
   group('Adjust amount', () {
@@ -37,11 +37,26 @@ void main() {
       expect(sut.amount, amountSnapshot);
     });
 
-    test('Decrease amount', () {});
+    test('Decrease amount', () {
+      sut.tryIncreaseAmount();
+      int amountSnapshot = sut.amount;
+      sut.tryDecreaseAmount();
+      expect(sut.amount, amountSnapshot - 1);
+    });
 
-    test('Decrease amount where current amount is equal to 1', () {});
+    test('Decrease amount where current amount is equal to 1', () {
+      int amountSnapshot = sut.amount;
+      bool result = sut.tryDecreaseAmount();
+      expect(result, false);
+      expect(sut.amount, amountSnapshot);
+    });
 
-    test('Set amount', () {});
+    test('Set amount', () {
+      int neededAmount = 10;
+      expect(sut.amount, 1);
+      sut.setAmount(amount: neededAmount);
+      expect(sut.amount, neededAmount);
+    });
 
     test('Set amount to negative quantity', () {});
 
